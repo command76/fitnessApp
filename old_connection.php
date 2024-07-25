bc7f37dabf2593b7f9e7a2a5fcd001ffd5972305
Anthony Taveras <urawesomeman@hotmail.com>
2024-07-25 09:15:28 -0400

    updated connection module, added env

---
+++ old_connection.php
@@ -0,0 +1,73 @@
+<?php 
+namespace dbconnecting;
+
+class connection {
+    private $pass = null;
+    private $user = null;
+    private $port = null;
+    private $host = null;
+    private $database = null;
+    public $conn = null;
+
+    public function connectionAttempt() {
+        $this->user = $this->getLoginInfo("user");
+        $this->pass = $this->getLoginInfo("pass");
+        $this->port = $this->getLoginInfo("port");
+        $this->host = $this->getLoginInfo("host");
+        $this->conn = new \mysqli($this->host, $this->user, $this->pass);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+          }
+          echo "Connected successfully";
+        $createNewDB = file_get_contents("./db_setup.sql");
+        $this->conn->multi_query("{$createNewDB}");
+    }
+
+    private static function getLoginInfo($type) {
+        if ($type === "user") {
+            $getUser = file_get_contents("./login_info.txt", true);
+            ob_start();
+             var_dump($getUser);
+             ob_get_clean();
+             preg_match("/(?<=user: ).*/", $getUser, $matches );
+             return $matches[0];
+        } elseif ($type === "pass") {
+            $getPass = file_get_contents("./login_info.txt", true);
+            ob_start();
+            var_dump($getPass);
+            ob_get_clean();
+            preg_match("/(?<=pass: ).*/", $getPass, $matches );
+            return $matches[0];
+        } elseif ($type === "port") {
+            $getPort = file_get_contents("./login_info.txt", true);
+            ob_start();
+            var_dump($getPort);
+            ob_get_clean();
+            preg_match("/(?<=port: ).*/", $getPort, $matches );
+            return $matches[0];
+        } else {
+            $getHost = file_get_contents("./login_info.txt", true);
+            ob_start();
+            var_dump($getHost);
+            ob_get_clean();
+            preg_match("/(?<=host: ).*/", $getHost, $matches );
+            return $matches[0];
+        }
+    }
+
+    private function getDatabase() {
+        $this->database = file_get_contents("./login_info.txt", true);
+        ob_start();
+        var_dump($this->database);
+        ob_get_clean();
+        preg_match("/(?<=database: ).*/", $this->database, $matches );
+        return $matches[0];
+    }
+
+    public function closeConnection() {
+        $this->conn->close();
+        echo "<p>Connection Closed</p>";
+    }
+}
+?>
