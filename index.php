895b7c0f762de69c580e7cdef6abec4f095d5a19
Anthony Taveras <urawesomeman@hotmail.com>
2024-08-01 11:18:58 -0400

    added ability to update all workouts for a user in workouts table (#9)

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <!DOCTYPE html>
-<?php // require "./connection.php";
-// require "./connection.php";
-// require "./connection.php";
-// require "./connection.php";
-// require "./connection.php";
-require dirname(__FILE__, 1) . "/forms/post_workouts.php";
-// use dbconnecting as DB;
-// $connectionObject = new DB\connection();
-// $connectionObject->connectionAttempt();
-// $connectionObject->closeConnection();
-?>
-<?php post_latest_workouts(1, 1); ?>
+<?php require dirname(__FILE__, 1) . "/forms/post_workouts.php"; ?>
+<?php post_latest_workouts_form(); ?>
